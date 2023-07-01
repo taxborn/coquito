7bf6aaa02c9809033bdc026dcbfa90d45ab059db
Braxton Fair <hello@taxborn.com>
2023-07-01 11:19:01 -0500

    feat: start basic argument parsing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void print_usage(char* executable) {
+    // TODO: argument parsing options
+    printf("[ERROR] Incorrect number of arguments passed.\n");
+    printf("[USAGE] %s <file.cq>\n", executable);
+
+    // Exit with an error
+    // TODO: Automatically go for main.cq? Seems unexpected but *could* be nice?
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char** argv) {
-    printf("hello, coquito.\n");
+    // Check if the correct number of arguments were passed.
+    // TODO: argument parsing
+    if (argc != 2) print_usage(argv[0]);
+
+    char* file_name = argv[1];
+    printf("[INFO] Starting compilation of '%s'.\n", file_name);
 
     return 0;
 }
