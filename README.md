bc9f21382ea2f503b67a539de861a2a4595a8bb5
Braxton Fair <hello@taxborn.com>
2023-07-01 11:07:44 -0500

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# :coconut: Coquito - an experimental programming language
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("hello, coquito.\n");
+
+    return 0;
+}
